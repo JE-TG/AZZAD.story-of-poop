d8b747f365d608e581a9792ec4c5c690dc9c970e
JE-TG <caspernholm3000@gmail.com>
2026-02-09 19:35:57 +0100

    Add files via upload

---
+++ Azads nettside/script.js
@@ -0,0 +1,17 @@
+
+
+const button = document.getElementById("button")
+
+function changeColor() {
+    document.body.style.backgroundColor = "#FAD223";
+    
+console.log(window.innerHeight);
+console.log(window.outerHeight);
+
+}
+
+
+
+
+
+
